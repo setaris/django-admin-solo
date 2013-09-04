438f0413f10f6b1da30ef8a19231bd08007a53cf
Joshua Fialkoff <joshua.fialkoff@setaris.com>
2013-09-04 11:12:51 -0400

    Setup file fix.

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ install_requires = [
 setup(
     name = "django-admin-solo",
     version = __version__,
-    description = "Just Django admin with some refactoring to make it DRYer."
-    url = "https://github.com/setaris/django-admin",
+    description = "Just Django admin with some refactoring to make it DRYer.",
+    url = "https://github.com/setaris/django-admin-solo",
     author = "Setaris",
     author_email = "support@setaris.com",
     packages = find_packages(),
